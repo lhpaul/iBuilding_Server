e1d85cd24e8912f4c2b58a0ea0a3b9c78c513df7
Luis Hernan Paul <lhpaul11@gmail.com>
2012-10-14 20:55:31 -0300

    partiendo

---
+++ iBuildingServer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IBuildingServer::Application.config.secret_token = '1c0f9cd1b15c3f2e8e17c7b871b96dfcbda39051dea10f93f649f58d0a1c03d4c0bc9f3fc1716ce8513bc66132eb486e14af8b20b6303c7ae5f4fc6e27d7e9ee'
