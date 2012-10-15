71e420d2341149a0d493e96b6361a2ffc27bee75
Luis Hernan Paul <lhpaul11@gmail.com>
2012-10-15 12:43:25 -0300

    ahora si creo

---
+++ config/mongohq.rb
@@ -0,0 +1,31 @@
+# gem install mongo bson_ext json
+require 'mongo'
+require 'uri'
+require 'json'
+
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+end
+
+db = get_connection
+
+puts "Collections"
+puts "==========="
+collections = db.collection_names
+puts collections
+
+last_collection = collections[-1]
+coll = db.collection(last_collection)
+
+# just show 5
+docs = coll.find().limit(5)
+
+puts "\nDocuments in #{last_collection}"
+puts "  #{docs.count()} documents(s) found"
+puts "=========================="
+docs.each{ |doc| puts doc.to_json }
