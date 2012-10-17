cc57906f4a75aedc2fefa7b80e2561d73ad5bb6a
Luis Hernan Paul <lhpaul11@gmail.com>
2012-10-17 17:34:03 -0300

    asdasdasd

--- config/application.rb
+++ config/application.rb
@@ -20,14 +20,14 @@ module Rails3MongoidDevise
   class Application < Rails::Application
 
     # don't generate RSpec tests for views and helpers
-    # config.generators do |g|
-# 
-      # g.test_framework :rspec, fixture: true
-      # g.fixture_replacement :factory_girl
-# 
-      # g.view_specs false
-      # g.helper_specs false
-    # end
+    config.generators do |g|
+
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl
+
+      g.view_specs false
+      g.helper_specs false
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
